ab2828dd356fbc2c45f684a0dbf4c7f58bb4cc6e
Iris <iris@raidev.eu>
2026-03-25 01:31:22 +0200

    push again

--- export/web/index.service.worker.js
+++ export/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1774394847|5263611951';
+const CACHE_VERSION = '1774395071|5487088387';
 /** @type {string} */
 const CACHE_PREFIX = 'HiShell-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
