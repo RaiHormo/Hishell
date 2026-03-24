9e2aa3e8fd8e5d997306f619e7a45679e9946457
Iris <iris@raidev.eu>
2026-03-24 13:00:43 +0200

    added compression

--- export/web/index.service.worker.js
+++ export/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1774348367|920674150';
+const CACHE_VERSION = '1774349728|2281615041';
 /** @type {string} */
 const CACHE_PREFIX = 'HiShell-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
