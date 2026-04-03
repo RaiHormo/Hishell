ffa146ad7f42cbc8aa61cc02259894cfaae410e0
Iris <iris@raidev.eu>
2026-04-03 12:59:39 +0300

    fixed script loading

--- export/web/index.service.worker.js
+++ export/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1775180605|7976329103';
+const CACHE_VERSION = '1775210361|2185270565';
 /** @type {string} */
 const CACHE_PREFIX = 'HiShell-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
